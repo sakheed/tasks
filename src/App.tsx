b2a6c12aa964177d502929d7425f50578e3409b3
sakheed <sakheed@udel.edu>
2023-02-14 14:43:33 -0500

    Task 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./App.css";
 
+import { Button, Col, Container, Row } from "react-bootstrap";
+
 function App(): JSX.Element {
     return (
         <div className="App">
@@ -9,6 +11,47 @@ function App(): JSX.Element {
             </header>
             <p>My name is Sakhee Desai</p>
             <p> Hello World</p>
+
+            <h1> My Favorite Things </h1>
+            <Container>
+                <Row>
+                    <Col>
+                        {" "}
+                        <img
+                            src="../assets/images/fiji.jpg"
+                            alt="A picture of a beach in Fiji"
+                        />
+                        <div
+                            style={{
+                                width: "100px",
+                                height: "50px",
+                                backgroundColor: "red"
+                            }}
+                        ></div>
+                    </Col>
+
+                    <Col>
+                        <ol>
+                            <li> Friends </li>
+                            <li> Traveling </li>
+                            <li> Dark Chocolate </li>
+                        </ol>
+
+                        <div
+                            style={{
+                                width: "100px",
+                                height: "50px",
+                                backgroundColor: "red"
+                            }}
+                        ></div>
+                    </Col>
+                </Row>
+            </Container>
+
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
+
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
